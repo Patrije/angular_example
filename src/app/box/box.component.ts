d4ac0b11b83a7a69a5a632296e4567f3667432de
patrycja <paatt10i@gmail.com>
2022-08-25 19:01:32 +0200

    example 3

--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Product{
+  name: string,
+  description: string
+}
+
 @Component({
   selector: 'app-box',
   templateUrl: './box.component.html',
@@ -7,9 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoxComponent implements OnInit {
 
+  products: Product[] = [{name: 'apple', description: 'sweet'}, {name: 'watermelon', description: 'waterful'}]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  share(){
+    console.log('click')
   }
 
 }
