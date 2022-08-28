4ba3d42c3530325c68c4276b92e3150143a07274
patrycja <paatt10i@gmail.com>
2022-08-28 12:11:42 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoxComponent } from './box/box.component';
 import { OwoceComponent } from './owoce/owoce.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
@@ -7,7 +8,8 @@ import { ToDoListComponent } from './to-do-list/to-do-list.component';
 const routes: Routes = [
   {path: 'owoce', component: OwoceComponent},
   {path: 'shopping', component: ShoppingListComponent},
-  {path: 'todos', component: ToDoListComponent}
+  {path: 'todos', component: ToDoListComponent},
+  {path: 'box', component: BoxComponent}
 ];
 
 @NgModule({
