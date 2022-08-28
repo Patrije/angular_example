6778aaab781b6afb89779ef2a96f95d1a4331caa
patrycja <paatt10i@gmail.com>
2022-08-28 12:00:13 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OwoceComponent } from './owoce/owoce.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'owoce', component: OwoceComponent},
+  {path: 'shopping', component: ShoppingListComponent},
+  {path: 'todos', component: ToDoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
