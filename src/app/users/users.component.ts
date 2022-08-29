268900cd79c631d5f9187c11974b73908231664b
patrycja <paatt10i@gmail.com>
2022-08-29 20:44:37 +0200

    typescript

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -9,12 +9,22 @@ import { User, UserService } from './service/user.service';
 export class UsersComponent implements OnInit {
 
   users: User[] = [];
+  numbers = [1,2,3,4];
 
   constructor(private service: UserService) {
    }
 
   ngOnInit(): void {
-    this.service.getUsers().subscribe(resposne => this.users = resposne);
+    this.service.getUsers().subscribe(resposne => this.users = resposne.filter(us => us.name.startsWith('C')));
   }
+
+    sumTable(){
+      return this.numbers.reduce((acc = 0, cur) => acc - cur)
+    }
+
+    add(){
+      return this.numbers.forEach(number => console.log(number));
+  
+    }
 
 }
